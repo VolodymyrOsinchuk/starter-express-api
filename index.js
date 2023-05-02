402e6a3b9d70a4e2071082bd323d9c0bf933f323
VolodymyrOsinchuk <osinchuk28@live.com>
2023-05-02 10:22:07 +0200

    update mai 2023

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Update in 02/05/23!");
+});
+app.listen(process.env.PORT || 3000);
